96d58325906ac9adbe97353026ca2afd77929c71
Jonny Burger <jonathanburger11@gmail.com>
2024-09-23 11:04:38 +0200

    Jumpcut example

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,52 @@
-export const MyComposition = () => {
-  return null;
+import { useMemo } from "react";
+import {
+  OffthreadVideo,
+  staticFile,
+  useCurrentFrame,
+  useVideoConfig,
+} from "remotion";
+
+export const JumpCuts = () => {
+  const { fps } = useVideoConfig();
+  const frame = useCurrentFrame();
+
+  const sections = useMemo(() => {
+    return [
+      { startFrom: 0, endAt: 5 * fps },
+      {
+        startFrom: 7 * fps,
+        endAt: 10 * fps,
+      },
+      {
+        startFrom: 13 * fps,
+        endAt: 18 * fps,
+      },
+    ];
+  }, [fps]);
+
+  const startFrom = useMemo(() => {
+    let summedUpDurations = 0;
+    for (const section of sections) {
+      summedUpDurations += section.endAt - section.startFrom;
+      if (summedUpDurations > frame) {
+        return section.endAt - summedUpDurations;
+      }
+    }
+
+    return null;
+  }, [frame, sections]);
+
+  if (startFrom === null) {
+    return null;
+  }
+
+  return (
+    <OffthreadVideo
+      pauseWhenBuffering
+      startFrom={startFrom}
+      // Remotion will automatically add a time fragment to the end of the video URL
+      // based on `startFrom` and `endAt`. Opt out of this by adding one yourself.
+      src={`${staticFile("time.mp4")}#t=0,`}
+    />
+  );
 };
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,13 +1,13 @@
 import { Composition } from "remotion";
-import { MyComposition } from "./Composition";
+import { JumpCuts } from "./Composition";
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
       <Composition
         id="MyComp"
-        component={MyComposition}
-        durationInFrames={60}
+        component={JumpCuts}
+        durationInFrames={30 * 30}
         fps={30}
         width={1280}
         height={720}
